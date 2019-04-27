710acee33a69a6e6a2a602f704ecaa3ac614fdd4
Charles NGUYEN <charles.nguyen@scality.com>
2019-04-27 23:58:10 +0200

    INIT

---
+++ interview/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import list from "./data";
+
+var newPerson = {
+  id: 0,
+  first_name: "Charles",
+  last_name: "Newen",
+  email: "charles@toto.com",
+  age: 30,
+  ip_address: "999.999.999.999"
+};
+
+function App() {
+  var [persons, setPersons] = useState(list);
+  return (
+    <div className="app">
+      <div className="container">
+        <img src={logo} className="logo" />
+        <div className="list">
+          {persons.map(i => (
+            <li>
+              {i.first_name} {i.last_name}
+            </li>
+          ))}
+        </div>
+      </div>
+      <div
+        class="button"
+        onClick={() => {
+          setPersons(persons.push(newPerson));
+        }}
+      >
+        Add
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ interview/src/data.js
@@ -0,0 +1,42 @@
+export const list = [
+  {
+    id: 1,
+    first_name: "Darryl",
+    last_name: "Quimby",
+    email: "dquimby0@zdnet.com",
+    age: 86,
+    ip_address: "143.3.138.115"
+  },
+  {
+    id: 2,
+    first_name: "Merci",
+    last_name: "Yesichev",
+    email: "myesichev1@tmall.com",
+    age: 58,
+    ip_address: "241.47.205.21"
+  },
+  {
+    id: 3,
+    first_name: "Sonja",
+    last_name: "Beefon",
+    email: "sbeefon2@wikimedia.org",
+    age: 14,
+    ip_address: "22.7.109.73"
+  },
+  {
+    id: 4,
+    first_name: "Gustavo",
+    last_name: "Jacquemy",
+    email: "gjacquemy3@sphinn.com",
+    age: 25,
+    ip_address: "209.71.77.7"
+  },
+  {
+    id: 5,
+    first_name: "Mignonne",
+    last_name: "Defew",
+    email: "mdefew4@pen.io",
+    age: 83,
+    ip_address: "227.57.86.244"
+  }
+];
